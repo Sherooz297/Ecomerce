759229822cfa71eac1b12a2802e80ebc9ba1dbde
Aimal Sherooz <aimalsherooz123@gmail.com>
2023-07-08 13:40:02 +0500

    creating update,delete,singleproduct route

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,5 +1,6 @@
 const Product = require('../Models/productModal')
 
+//Creating the New product ---Admin Only--- 
 
 exports.createproduct = async(req,res,next)=>{
     const product = await Product.create(req.body)
@@ -9,7 +10,8 @@ exports.createproduct = async(req,res,next)=>{
     })
 
 }
-
+ 
+//   Getting   all  product  
 
 exports.getallproduct=async(req,res)=>{
    const product = await Product.find()
@@ -24,3 +26,58 @@ exports.getallproduct=async(req,res)=>{
     product
    })
 }
+
+ //   Updating the  product  ---admin  only---
+  
+ exports.updateproduct = async (req,res)=>{
+
+    let product = await Product.findById(req.params.id);
+    if(!product){
+        res.status(400).json({
+            success:false,
+            message:"product not found"
+        })
+    }
+      product=await Product.findByIdAndUpdate(req.params.id,req.body)
+      res.status(200).json({
+        success:true,
+        message:"product updated"
+      })
+ }
+
+  //   Delete the  product  ---admin  only---
+
+  exports.deleteproduct = async(req,res) =>{
+
+    const product =await Product.findByIdAndDelete(req.params.id)
+    if(!product){
+        res.status(400).json({
+            success:false,
+            message:"product not found"
+        })
+    }
+     
+      res.status(200).json({
+        success:true,
+        message:"product deleted"
+      })
+  }
+
+
+   //   getting single product  
+
+   exports.getsingleproduct = async(req,res,next) =>{
+    const product = await Product.findById(req.params.id)
+    if(!product){
+        res.status(400).json({
+            success:false,
+            message:"product not found"
+        })
+    }
+
+    res.status(200).json({
+        success:true,
+        product
+    })
+
+   }
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,10 +1,13 @@
 const express = require('express')
 
-const {getallproduct, createproduct} = require("../controller/productController")
+const {getallproduct, createproduct, updateproduct, deleteproduct, getsingleproduct} = require("../controller/productController")
 const router = express.Router()
 
 router.route("/product").get(getallproduct)
 router.route("/createproduct").post(createproduct)
+router.route("/updateproduct/:id").put(updateproduct)
+router.route("/deleteproduct/:id").delete(deleteproduct)
+router.route("/getsingleproduct/:id").get(getsingleproduct)
 
 
 module.exports = router
